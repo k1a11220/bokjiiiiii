692d286e3da71d553a8209bc8bff7b70e2618288
Beomsoo <k1a11220@noreply.github.com>
2021-01-27 00:19:30 +0900

    Fix cors error
    Add https://cors-anywhere.herokuapp.com/

---
+++ test.js
@@ -0,0 +1,18 @@
+// let request = require("request");
+// let cheerio = require("cheerio");
+
+// const API_KEY =
+//   "qt%2BHSez0GjoTYvzN9D49cDlbHcVmSSxlKAUmIrLEqtHhnJBf6URyeHVJYuTIP13Hm148dCo3U7qu0QeSF%2FXoZw%3D%3D";
+
+// const LIFE_ARRAY = "004";
+
+// const CHAR_ARRAY = "006";
+// const INDVD_ARRAY = "001";
+// const DESIRE_ARRAY = "7000000";
+
+// const url = `https://www.bokjiro.go.kr/openapi/rest/gvmtWelSvc?crtiKey=${API_KEY}&callTp=L&pageNum=1&numOfRows=100&lifeArray=${LIFE_ARRAY}&charTrgterArray=${CHAR_ARRAY}&trgterIndvdlArray=${INDVD_ARRAY}&desireArray=${DESIRE_ARRAY}`;
+
+// request(url, function (err, res, body) {
+//   $ = cheerio.load(body);
+//   console.log($);
+// });
